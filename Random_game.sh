2bef155dec9125dd059d6e4745249b9858e4d411
KBrand <kbrandn25@gmail.com>
2025-11-20 12:01:50 +0100

    new properties

--- Random_game.sh
+++ Random_game.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 x=$(( 0 + RANDOM % (5 - 0 + 1) ))
+count=0
 echo "=========================================="
-echo "   Guest a number to win between 0 to 5:  "
+echo "   Guest a number to win between 0 to max you want:  "
 echo "=========================================="
+read -p "Enter the number of guest you want to play: " upper_limit
+for i in $(seq 1 "$upper_limit")
+ do
  read -p "Enter the value you guest: " y 
  if [[ $y -eq $x ]] then 
  echo "You WIN"
+ count++;
   elif [[ $y -lt $x ]] then
    echo "You Lose!"
-   echo "you below the win number"
+   #echo "you below the win number"
    elif [[ $y -gt $x ]] then
    echo "You Lose!"
-   echo "you are above the win number"
+   #echo "you are above the win number"
 else
   echo "Invalid number, Not within the range"
 fi
 echo "the win answer is $x "
+done
+echo "Your rsult"
+echo "----Your total win count is $count/$upper_limit-----"
